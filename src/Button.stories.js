07ee1549fc0d02bc59853c2dd9bb93c32fe631b0
Falk Köppe <falk.koeppe@gmail.com>
2022-05-07 20:29:35 +0200

    Add button story with interactions

--- src/Button.stories.js
+++ src/Button.stories.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { userEvent, within } from "@storybook/testing-library";
 
 import { Button } from "./Button";
 import { Icon } from "./Icon";
@@ -325,3 +326,22 @@ export const AnchorWrapper = (args) => (
 );
 
 AnchorWrapper.storyName = "anchor wrapper";
+
+// New story using the play function
+export const WithInteractions = () => (
+  <Button
+    ButtonWrapper={StoryLinkWrapper}
+    appearance="primary"
+    size="small"
+    href="http://storybook.js.org"
+  >
+    Button
+  </Button>
+);
+WithInteractions.play = async ({ canvasElement }) => {
+  // Assigns canvas to the component root element
+  const canvas = within(canvasElement);
+  await userEvent.click(canvas.getByRole("link"));
+};
+
+WithInteractions.storyName = "button with interactions";
